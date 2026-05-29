9bda8777ce0cb5c17e3b26b438e581e7709020a7
Shuvam <kundushibam7@gmail.com>
2026-05-29 13:44:58 +0530

    user model update

--- server/models/User.js
+++ server/models/User.js
@@ -6,6 +6,9 @@ const userSchema = new mongoose.Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     credits: {type: Number, default: 20},
+},{
+    timestamps: true,
+    bufferCommands: false
 })
 
 //Hash password before saving 
