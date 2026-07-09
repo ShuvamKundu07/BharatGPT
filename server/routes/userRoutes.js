9e7bbe6965cec33d9d08a2408beaec2280ab081f
Shuvam <kundushibam7@gmail.com>
2026-07-09 22:06:31 +0530

    add API prefix to user routes for consistency

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,9 +4,9 @@ import { protect } from "../middlewares/auth.js";
 
 const userRouter = express.Router();
 
-userRouter.post('/register', registerUser)
-userRouter.post('/login', loginUser)
-userRouter.get('/data', protect, getUser)
-userRouter.get('/published-images', getPublishedImages)
+userRouter.post('/api/register', registerUser)
+userRouter.post('/api/login', loginUser)
+userRouter.get('/api/data', protect, getUser)
+userRouter.get('/api/published-images', getPublishedImages)
 
 export default userRouter;
