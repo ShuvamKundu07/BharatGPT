0f07b2c5944974e43232f2c4c3bea0213602db5a
Shuvam <kundushibam7@gmail.com>
2026-07-10 20:34:39 +0530

    refactor CORS middleware to simplify origin handling

--- server/server.js
+++ server/server.js
@@ -21,23 +21,9 @@ app.post('/api/stripe',express.raw({type: 'application/json'}),stripeWebhooks)
 // console.log("PUBLIC =", process.env.IMAGEKIT_PUBLIC_KEY);
 
 
-// Middleware
-const allowedOrigins = [
-    'http://localhost:5173', // Vite local development
-    'https://bharat-gpt-server-e5ptcwcxp-shuvam-kundus-projects.vercel.app/' // Vercel production deployment
-];
-
+//Middleware
 app.use(cors({
-    origin: function (origin, callback) {
-        // Allow requests with no origin (like mobile apps, curl, or server-to-server)
-        if (!origin) return callback(null, true);
-        
-        if (allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: 'https://bharat-gpt-server-nocx4ykoo-shuvam-kundus-projects.vercel.app/', // Your backend url
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
