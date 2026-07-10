b522c759d05c9ab53fcb50a4e1d552305516487d
Shuvam <kundushibam7@gmail.com>
2026-07-10 20:41:06 +0530

    removed slash

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.post('/api/stripe',express.raw({type: 'application/json'}),stripeWebhooks)
 
 //Middleware
 app.use(cors({
-    origin: 'https://bharat-gpt-client.vercel.app/', // Your frontend url
+    origin: 'https://bharat-gpt-client.vercel.app', // Your frontend url
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
